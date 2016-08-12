3d92b8cafb73533f950fc80bf3cb5e3619bf7d39
guojian83 <guojian83@yahoo.com>
2016-08-12 11:52:58 +1000

    translation and fixed sms dialog label

--- erpnext/public/js/sms_manager.js
+++ erpnext/public/js/sms_manager.js
@@ -71,10 +71,10 @@ function SMSManager(doc) {
 	}
 	this.make_dialog = function() {
 		var d = new frappe.ui.Dialog({
-			title: 'Send SMS',
+			title: __('Send SMS'),
 			width: 400,
 			fields: [
-				{fieldname:'number', fieldtype:'Data', label:'Mobile Number', reqd:1},
+				{fieldname:'number', fieldtype:'Data', label:__('Mobile Number'), reqd:1},
 				{fieldname:'message', fieldtype:'Text', label:'Message', reqd:1},
 				{fieldname:'send', fieldtype:'Button', label:'Send'}
 			]
